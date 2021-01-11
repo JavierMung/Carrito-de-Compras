ec170c7fefb9a8d44fd4e0be0073ca8f36e9635d
Marco <Marco@DESKTOP-8H9GODC>
2021-01-10 22:20:13 -0600

    Joption added

--- Carrito_de_Compras/src/carrito_de_compras/Carrito_de_Compras.java
+++ Carrito_de_Compras/src/carrito_de_compras/Carrito_de_Compras.java
@@ -1,12 +1,15 @@
 
 package carrito_de_compras;
 
+import javax.swing.JOptionPane;
+
 
 public class Carrito_de_Compras {
 
     
     public static void main(String[] args) {
        
+        JOptionPane.showConfirmDialog(null, "Huevos");
         //JAVIER ES PUTOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
     }
     
